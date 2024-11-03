e1c2e576c5cfabe98aec88d0a11c3980e97d27f5
mshahzebraza <m.shahzebraza.97@gmail.com>
2024-11-03 16:35:24 +0500

    connect scraper with server endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 // src/index.ts
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
+import crawler from "./scraper.js";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+
+app.get("/scrape", async (_req: Request, res: Response) => {
+    const startUrls = ["https://crawlee.dev"];
+    await crawler.run(startUrls);
+    await crawler.teardown();
+    res.send("Scrape");
+});
 
 app.get("/", (_req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
--- src/scraper.ts
+++ src/scraper.ts
@@ -3,13 +3,12 @@ import { PuppeteerCrawler, log } from 'crawlee';
 
 import { router } from './routes.js';
 
-const startUrls = ['https://crawlee.dev'];
 
 const crawler = new PuppeteerCrawler({
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
     // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 10,
+    maxRequestsPerCrawl: 2,
 });
 
-await crawler.run(startUrls);
+export default crawler;
